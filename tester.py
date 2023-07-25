b547f2ef08fb4c39729988e0f903c4d2342624a8
OldTyT <nezlobin118@gmail.com>
2023-07-26 00:19:01 +0800

    fix: message when master node tests

--- tester.py
+++ tester.py
@@ -21,7 +21,7 @@ def get_tests_cmd(hosts: list) -> list:
     if os.getenv("WORKER_NODES_LIST") is not None:
         cmd_list.append(f"test -f testinfra_worker.py && printf '\033[0;32m\n{separator}\nTesting: worker\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts={os.getenv('WORKER_NODES_LIST')} --ansible-inventory=inventory.yml --connection=ansible  testinfra_worker.py || printf '\033[0;33m\n{separator}\nWarning!\nNot found test for: worker\n{separator}\033[0m\n'")
     if os.getenv("MASTER_NODES_LIST") is not None:
-        cmd_list.append(f"test -f testinfra_master.py && printf '\033[0;32m\n{separator}\nTesting: worker\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts={os.getenv('MASTER_NODES_LIST')} --ansible-inventory=inventory.yml --connection=ansible  testinfra_master.py || printf '\033[0;33m\n{separator}\nWarning!\nNot found test for: master\n{separator}\033[0m\n'")
+        cmd_list.append(f"test -f testinfra_master.py && printf '\033[0;32m\n{separator}\nTesting: master\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts={os.getenv('MASTER_NODES_LIST')} --ansible-inventory=inventory.yml --connection=ansible  testinfra_master.py || printf '\033[0;33m\n{separator}\nWarning!\nNot found test for: master\n{separator}\033[0m\n'")
     if os.getenv("TEST_ALL_DISABLED") != "True":
         cmd_list.append(f"printf '\033[0;32m\n{separator}\nTesting: all\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts=all --ansible-inventory=inventory.yml --connection=ansible  testinfra_all.py")
     return cmd_list
