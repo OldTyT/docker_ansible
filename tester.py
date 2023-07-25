0fa8611f44ccbbbc8efe1717416737e49f252c31
OldTyT <nezlobin118@gmail.com>
2023-07-26 00:07:36 +0800

    fix: syntax

--- tester.py
+++ tester.py
@@ -17,9 +17,9 @@ def get_tests_cmd(hosts: list) -> list:
     cmd_list = []
     for host in hosts:
         cmd_list.append(f"test -f testinfra_{host}.py && printf '\033[0;32m\n{separator}\nTesting: {host}\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts={host} --ansible-inventory=inventory.yml --connection=ansible  testinfra_{host}.py || printf '\033[0;33m\n{separator}\nWarning!\nNot found test for: {host}\n{separator}\033[0m\n'")
-    if os.getenv("WORKER_NODES_LIST") not is None:
+    if os.getenv("WORKER_NODES_LIST") is not None:
         cmd_list.append(f"test -f testinfra_worker.py && printf '\033[0;32m\n{separator}\nTesting: worker\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts={os.getenv("WORKER_NODES_LIST")} --ansible-inventory=inventory.yml --connection=ansible  testinfra_worker.py || printf '\033[0;33m\n{separator}\nWarning!\nNot found test for: worker\n{separator}\033[0m\n'")
-    if os.getenv("MASTER_NODES_LIST") not is None:
+    if os.getenv("MASTER_NODES_LIST") is not None:
         cmd_list.append(f"test -f testinfra_master.py && printf '\033[0;32m\n{separator}\nTesting: worker\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts={os.getenv("MASTER_NODES_LIST")} --ansible-inventory=inventory.yml --connection=ansible  testinfra_master.py || printf '\033[0;33m\n{separator}\nWarning!\nNot found test for: master\n{separator}\033[0m\n'")
     if os.getenv("TEST_ALL_DISABLED") != "True":
         cmd_list.append(f"printf '\033[0;32m\n{separator}\nTesting: all\n{separator}\n\033[0m\n' && py.test -v --color=yes --hosts=all --ansible-inventory=inventory.yml --connection=ansible  testinfra_all.py")
