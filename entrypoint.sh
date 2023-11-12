8beba1267166da108ce5ce3e8b9406fbed2c4118
Old TyT <42464565+OldTyT@users.noreply.github.com>
2023-11-12 19:48:46 +0300

    chore: update ansible args

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ echo "$SSH_KEY_PRIVATE"|base64 -d > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
 if [ "$TYPE" = "diff" ]; then
-    ansible-playbook --diff -i ./inventory.yml playbook.yml && exit
+    ansible-playbook --check --diff -i ./inventory.yml playbook.yml && exit
 fi
 if [ "$TYPE" = "test" ]; then
     python3 /tester.py | bash && exit
 fi
 if [ "$TYPE" = "play" ]; then
-    ansible-playbook -i ./inventory.yml playbook.yml && exit
+    ansible-playbook --diff -i ./inventory.yml playbook.yml && exit
 else
     echo "Invalid env value - $TYPE"
     exit 1
