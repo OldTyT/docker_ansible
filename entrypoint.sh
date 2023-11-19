b09c0f06f7ab0c12c225d7a0d160f50af0d1ce08
Old TyT <42464565+OldTyT@users.noreply.github.com>
2023-11-19 15:29:02 +0300

    feat: added download requirements

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 echo "$SSH_KEY_PRIVATE"|base64 -d > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
+test -f requirements.yml && ansible-galaxy install -r requirements.yml
 
 if [ "$TYPE" = "diff" ]; then
     ansible-playbook --check --diff -i ./inventory.yml playbook.yml && exit
